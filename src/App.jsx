bf32bfb5bd7e4fe5c52554a6d29e52ebfa331fa6
bvgross <bvgross@gmx.com>
2023-03-20 17:06:39 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+    return (
+        <div>
+            <header>
+                <div>Registro</div>
+                <div>Visualização</div>
+            </header>
+            <main>
+
+            </main>
+        </div>
+    );
+};
+
+export default App;
