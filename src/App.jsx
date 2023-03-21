29e0c5695ae12ae4a14731441ed58f4ce55cb184
bvgross <bvgross@gmx.com>
2023-03-21 17:34:58 -0300

    Prototyping layout
    Trying to find the best layout for usability

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 function App() {
     return (
         <div>
-            <header>
-                <div>Registro</div>
-                <div>Visualização</div>
-            </header>
+            <nav className="nav">
+                <section className="nav-links">
+                    <div className="nav-links__link">Track</div>
+                    <div className="nav-links__link">Analize</div>
+                </section>
+                <i className="nav--menu fa fa-bars" aria-hidden="true"></i>
+            </nav>
             <main>
-
+                <div className="card">
+                    <label className="card-label" htmlFor="name">Exercise name</label>
+                    <input className="card-input" id="name" type="text" />
+                </div>
             </main>
         </div>
     );
